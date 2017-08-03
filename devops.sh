49e45e19dcbc176c615a1f449d463280017e9dfc
dado <littlevwing@live.com>
2017-08-04 01:49:51 +0200

    example ds, jsf works

--- devops.sh
+++ devops.sh
@@ -4,4 +4,6 @@ echo "ho ho!"
 
 echo "docker run -it -p 8080:8080 koma-standalone"
 
+echo "docker run --name koma-mysql -e MYSQL_ROOT_PASSWORD=koma-mysql -d mysql:latest"
 
+echo "docker run --name some-app --link some-mysql:mysql -d application-that-uses-mysql"
